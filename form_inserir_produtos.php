22ac359e9faf5f4e3024f6152728ab47d4c7bbfa
Wesley Pereira <138945186+wesleydev1BH@users.noreply.github.com>
2024-03-13 21:48:01 -0300

    Add files via upload

---
+++ form_inserir_produtos.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inserir Produtos</title>
+</head>
+<body>
+    <form action="inserir_produtos.php" method="get">
+        <label for="nome">Nome:</label>
+        <input type="text" value="nome"><br><br>
+
+        <label for="descricao">Descrição:</label>
+        <input type="text" value="descricao"><br><br>
+
+        <label for="preco">Preço:</label>
+        <input type="text" value="preco"><br><br>
+
+       <select name="exibir_categorias" id="">
+            <?php
+                $pdo = new PDO("sqlite:estoque.db");
+                $query_select_categorias = "SELECT * FROM categorias";
+
+                $categorias = $pdo->query($query_select_categorias);
+                foreach ($categorias as $categoria){
+                    echo "<option value=\"".$categoria["id"]
+                    ."\">".$categoria["nome"]."</option>";
+                }
+
+            ?>
+            
+       </select>
+       <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
